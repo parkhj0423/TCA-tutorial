c43d606320184f84d15767c0a39f4648add865ca
Parkhyunwoo <parkhj0423@naver.com>
2022-11-02 22:36:59 +0900

    [parkhj0423] initial commit

---
+++ TCA-Tutorial/TCA_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_TutorialApp.swift
+//  TCA-Tutorial
+//
+//  Created by 박현우 on 2022/11/02.
+//
+
+import SwiftUI
+
+@main
+struct TCA_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
